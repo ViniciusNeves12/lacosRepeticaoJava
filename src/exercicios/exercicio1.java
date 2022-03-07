0f3484174c9c4915b0e8baefb0a162ba43954c29
ViniciusNeves12 <viniciusneves1312@hotmail.com>
2022-03-07 05:03:44 -0300

    first commit

---
+++ src/exercicios/exercicio1.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1000; i < 2000; i++ ) {
+			if(i % 11 == 5) {
+				System.out.println(i + ", ");
+			}
+		}
+	}
+
+}
